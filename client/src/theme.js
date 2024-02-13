5378ec17dd61f212f3a2bdb15f9854ed224ff556
TheGrayGodfather <milanmohapatra.official@gmail.com>
2024-02-13 17:21:25 +0530

    project setup

---
+++ client/src/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            dark: "#D08C00",
+            main: "#FCA311",
+            light: "#FFD699"
+        },
+        secondary: {
+            dark: "#0E172B",
+            main: "#14213D",
+            light: "#6A7EAB"
+        }
+    }
+})
+
+export default theme
